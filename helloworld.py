3be6b1f133828c46ed16a433c0f2e77fdb187e6b
prudhvisurya996 <prudhvisurya996@gmail.com>
2023-12-04 12:32:06 +0530

    printing string integer commenting mulipe lines and single line

---
+++ helloworld.py
@@ -0,0 +1,32 @@
+print("hello world")
+
+# how to print I am " PRUDHVI SURYA"
+
+print('I am "PRUDHVI SURYA"')
+# how to print I am 'PRUDHVI SURYA '
+print("I am 'PRUDHVI SURYA'")
+a = 5
+print(a)
+b = "surya"
+c = 'surya'
+print(b, c)
+
+#How to add muliple comments use " " " three double quotes
+# without space or three single quotes without space ''' before and after
+"""
+commenting mutliple lines with double quotes
+observations
+ Mistakes
+ print(" Iam "prudhvi surya"")
+"""
+
+'''
+commenting muliple lines  using single quotes
+I am prudhvi surya
+'''
+
+
+
+
+
+
