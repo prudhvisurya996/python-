9fa6a96f640bf0399c95aea35ca20a0a57ad87b9
prudhvisurya996 <prudhvisurya996@gmail.com>
2023-12-04 12:56:30 +0530

    int float and print function

---
+++ int-float-input-newline.py
@@ -0,0 +1,35 @@
+"""
+here we use int,float and how to add new line in a print statement
+"""
+a = '123'
+b = int(a)
+print(b)
+print(type(a))
+print(type(b))
+
+"""
+ouptut
+123
+<class 'str'>
+<class 'int'>
+"""
+c = '123'
+d = float(c)
+print(d)
+print(type(c))
+print(type(d))
+"""
+output
+123.0
+<class 'str'>
+<class 'float'>
+"""
+print("I am surya\n I am from HYDERABAD")
+"""
+output
+I am surya
+ I am from HYDERABAD---space before I because we have given space after "\n" in 
+ print statement
+"""
+
+
