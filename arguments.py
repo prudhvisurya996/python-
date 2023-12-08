813e5afcf0a553c323eeb89d784d2b3a171bf189
prudhvisurya996 <prudhvisurya996@gmail.com>
2023-12-08 17:40:07 +0530

    arguments

---
+++ arguments.py
@@ -0,0 +1,22 @@
+def arith(a, b):
+    print(a * b)
+    print(a + b)
+    print(a - b)
+    print(a / b)
+
+a = int(input('enter first number\n'))
+b = int(input('Enter second number\n'))
+
+
+arith(a, b)
+
+"""
+enter first number
+10
+Enter second number
+2
+20
+12
+8
+5.0
+"""
